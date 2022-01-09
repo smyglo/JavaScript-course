4dc9b6833dfe1e6618e107802e184010accf5c83
Michal <michal.badzio@olx.pl>
2022-01-09 18:28:50 +0100

    End of part 1

--- 01-Fundamentals-Part-1/starter/coding_challenges.js
+++ 01-Fundamentals-Part-1/starter/coding_challenges.js
@@ -171,7 +171,7 @@
 
 // console.log(tip);
 
-const bill = 430;
-const tip = bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2
+// const bill = 430;
+// const tip = bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2
 
-console.log(`The bill was ${bill}, the tip was ${tip}, and the total value is ${bill + tip}.`)
+// console.log(`The bill was ${bill}, the tip was ${tip}, and the total value is ${bill + tip}.`)
