55614bff41edd69730873b515d7de7f3a21be88a
Michal <michal.badzio@olx.pl>
2022-01-27 16:01:55 +0100

    calcGross added

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -1,6 +1,6 @@
 // Remember, we're gonna use strict mode in all scripts now!
 'use strict';
 
-const calcAge = birthYear => 2022 - birthYear;
+// const calcAge = birthYear => 2022 - birthYear;
 
-console.log(calcAge(1992));
+// console.log(calcAge(1992));
