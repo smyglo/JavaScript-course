0dc5671026eb72f072d75c5e4eee81f06cac7685
Michal <michal.badzio@olx.pl>
2022-01-28 22:18:20 +0100

    Problem solving - done

--- 03-Developer-Skills/starter/calcGrossRO.js
+++ 03-Developer-Skills/starter/calcGrossRO.js
@@ -1,31 +1,31 @@
 'use-strict';
 
-const calcGros = function (nett) {
-  return nett * 0.19 + nett;
-};
+// const calcGros = function (nett) {
+//   return nett * 0.19 + nett;
+// };
 
-// console.log(calcGros(nett));
+// // console.log(calcGros(nett));
 
-const values = [824.99, 149.99, 149.99];
-const gross = [];
+// const values = [824.99, 149.99, 149.99];
+// const gross = [];
 
-for (let i = 0; i < values.length; i++) {
-  gross.push(calcGros(values[i]));
-}
+// for (let i = 0; i < values.length; i++) {
+//   gross.push(calcGros(values[i]));
+// }
 
-console.log(gross);
+// console.log(gross);
 
-const sumGross = function (grossValues) {
-  let sum = 0;
-  for (i = 0; i < grossValues.length; i++) {
-    sum += grossValues[i];
-  }
-  return sum;
-};
+// const sumGross = function (grossValues) {
+//   let sum = 0;
+//   for (i = 0; i < grossValues.length; i++) {
+//     sum += grossValues[i];
+//   }
+//   return sum;
+// };
 
-// First way - (nett to gross1) + (nett to gross2) = invoiceValue
-console.log(sumGross(gross));
+// // First way - (nett to gross1) + (nett to gross2) = invoiceValue
+// console.log(sumGross(gross));
 
-// Second way - (net1  + nett2) to gross = invoiceValue )
-const sumNett = sumGross(values);
-console.log(calcGros(sumNett));
+// // Second way - (net1  + nett2) to gross = invoiceValue )
+// const sumNett = sumGross(values);
+// console.log(calcGros(sumNett));
--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -4,3 +4,67 @@
 // const calcAge = birthYear => 2022 - birthYear;
 
 // console.log(calcAge(1992));
+
+// Using Google, StackOverflow and MDN
+
+// PROBLEM 1:
+
+// We work for a company building a smart home thermometer. Our most recent task is this: "Given an array of temperatures of one day, calculate the temperature amplitude. Keep in mind that sometimes there might be a sensor error."
+
+// const temperatures = [3, -2, -6, -1, 'error', 9, 13, 17, 15, 14, 9, 5];
+
+// #1 Understanding the problem
+// - What is temperature amplitude?
+//  Answer: difference between higfhes and lowest temp
+// - How to compute max and min temp?
+// - What is a sensor error? What to do when it occurs?
+//  Answers: Ignore the error, it is string: 'error'
+
+// #2 Breaking the problem into smaller sub-problems
+//  - How to ignore errors?
+//  - Find max value in the temp array
+//  - Find min value in the temp array
+//  - Subtract min from max (amplitude) and return it
+
+// const calcTempAmplitude = function (temps) {
+//   let max = temps[0];
+//   let min = temps[0];
+
+//   for (let i = 0; i < temps.length; i++) {
+//     const currTemp = temps[i];
+//     if (typeof currTemp !== 'number') continue;
+//     if (max < currTemp) max = currTemp;
+//     if (min > currTemp) min = currTemp;
+//   }
+//   return max - min;
+// };
+// const amplitude = calcTempAmplitude(temperatures);
+// console.log(amplitude);
+
+// // PROBLEM 2
+
+// // Function should recieve two arrays of temperatures
+// // #1 Understanding the problem
+// // - With two arrays should we implement functionality twice?
+// // Answer: NO. Just merge two arrays.
+
+// // #2 Breaking the problem into smaller sub-problems
+// // - How to merge two arrays?
+
+// const calcTempAmplitudeNew = function (t1, t2) {
+//   const temps = t1.concat(t2);
+//   console.log(temps);
+
+//   let max = temps[0];
+//   let min = temps[0];
+
+//   for (let i = 0; i < temps.length; i++) {
+//     const currTemp = temps[i];
+//     if (typeof currTemp !== 'number') continue;
+//     if (max < currTemp) max = currTemp;
+//     if (min > currTemp) min = currTemp;
+//   }
+//   return max - min;
+// };
+// const amplitudeNew = calcTempAmplitudeNew([3, 5, 6, -10], [1, 10, 111]);
+// console.log(amplitudeNew);
