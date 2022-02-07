6069b529dfbe3b4f7ea5b11662eeeb3daa64b48c
smyglo <michal.badzio.smyglo@gmail.com>
2022-02-07 22:17:34 +0100

    Modal 'esc' button functionality added - finished

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -6,7 +6,6 @@ const btnCloseModal = document.querySelector('.close-modal');
 const btnShowModal = document.querySelectorAll('.show-modal');
 
 const openModal = function () {
-  // console.log('Button clicked');
   modal.classList.remove('hidden');
   overlay.classList.remove('hidden');
 };
@@ -20,5 +19,10 @@ for (let i = 0; i < btnShowModal.length; i++)
   btnShowModal[i].addEventListener('click', openModal);
 
 btnCloseModal.addEventListener('click', closeModal);
-
 overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
